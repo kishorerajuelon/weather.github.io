526e64619dc1bc8dd24501ee80515017d16b2c6e
kishorerajuelon <86884069+kishorerajuelon@users.noreply.github.com>
2021-08-24 17:34:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ function showError(error){
 
 
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `HTTPS://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
     fetch(api)
         .then(function(response){
